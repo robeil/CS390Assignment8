54eb020d9009c9c8d9a251b63ab7f8c4a105f4f1
Robeil Aregawi <silukeen1@gmail.com>
2022-03-09 10:54:22 -0600

    updating

--- prob3/Marketing.java
+++ prob3/Marketing.java
@@ -1,9 +1,6 @@
 package CS390Assignment8.prob3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Marketing {
 	private String employeename;
@@ -52,9 +49,7 @@ public class Marketing {
 		if(!(obj instanceof Marketing)) return false;
 		Marketing m = (Marketing) obj;
 
-		 if(this.salesamount == m.salesamount &&
-				this.employeename.equalsIgnoreCase(m.employeename) &&
-				this.productname.equalsIgnoreCase(m.productname)){
+		 if(this.salesamount == m.salesamount){
 			 return true;
 		 } else {
 			 return false;
@@ -95,7 +90,20 @@ public class Marketing {
 		System.out.println("After sorting");
 		Collections.sort(marketList,new ComparatorClass());
 		System.out.println(marketList);
-		System.out.println("The highest sales amount");
+		System.out.println("The over 1000 sale-amount list after sorting");
 		System.out.println("This the new List " +listMoreThan1000(marketList));
+		System.out.println("Checking the result ");
+		System.out.println("object 0 equal to object index 1 is : " +marketList.get(0).equals(marketList.get(1)));
+		System.out.println("object 0 compare to object index 1 is : " +new ComparatorClass().compare(marketList.get(0),marketList.get(1)));
+		List<Marketing> list = listMoreThan1000(marketList);
+		//sorting the over1000 list
+		Collections.sort(list, new Comparator<Marketing>() {
+			@Override
+			public int compare(Marketing o1, Marketing o2) {
+				return o1.getEmployeename().compareTo(o2.employeename);
+			}
+		});
+		System.out.println("The over 1000 sale-amount list after sorting");
+		System.out.println(list);
 	}
 }
