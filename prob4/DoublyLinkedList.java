482a7cffe6fade343d88946dbda8f83fbc89dc99
Robeil Aregawi <silukeen1@gmail.com>
2022-03-09 10:53:02 -0600

    solved all the problems

--- prob4/DoublyLinkedList.java
+++ prob4/DoublyLinkedList.java
@@ -1,20 +1,59 @@
 package CS390Assignment8.prob4;
 
 public class DoublyLinkedList {
-	
+		public int size;
 		Node header;
+
 		DoublyLinkedList(){
 			header = new Node();
 		}
 		//adds to the end of the list
 		public void addLast(String item){
-			//implement
-			
+			//Creating new node
+			Node current = new Node();
+			if(header.next == null){
+				//make connection to the header.next
+				current.next = null;
+				current.previous = header;
+				header.next = current;
+				//assigning value
+				current.value = item;
+				size++;
+			} else {
+				Node temp = header.next;
+			while(temp.next != null){
+				temp = temp.next;
+			}
+			current.previous = temp;
+			temp.next = current;
+			current.value = item;
+			current.next = null;
+			size++;
+			}
 		}
 		public boolean remove(String item){
+			if(item == null) return false;
+			if(header.next == null){
+				return false;
+			} else {
+				Node current = header;
+				while(current != null){
+					if(item.equals(current.value) ){
+						if(current.next != null) {
+							current.next.previous = current.previous;
+							current.previous.next = current.next;
+							size--;
+						} else {
+							current.previous.next = null;
+						}
+						return true;
+					}
+					current = current.next;
+				}
+			}
 			return false;
-			//Implement
 		}
+
 		@Override
 		public String toString() {
 			
@@ -30,10 +69,11 @@ public class DoublyLinkedList {
 		}
 		
 		class Node {
+
 			String value;
 			Node next;
 			Node previous;
-			
+
 			public String toString() {
 				return value == null ? "null" : value;
 			}
@@ -45,5 +85,12 @@ public class DoublyLinkedList {
 			list.addLast("Harry");
 			list.addLast("Steve");
 			System.out.println(list);
+			System.out.println(list.size);
+			list.remove("Harry");
+			list.remove("Bob");
+			list.remove("Steve");
+			System.out.println(list);
+			System.out.println(list.size);
+
 		}
 }
